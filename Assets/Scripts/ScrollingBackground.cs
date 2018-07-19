c7417c5bf0efa2c75812de27bb0ef34120409784
Frey Kjelstrøm <freykjelstroem@gmail.com>
2018-07-19 23:09:33 +0200

    Planet Animation
    Added rotationg planet animation
    Added parallax scrolling background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public MeshRenderer meshRenderer;
+    public Transform player;
+
+    Vector2 startPos;
+
+    void Start () {
+        transform.position = new Vector3(0f, player.position.y, 0f);
+        startPos = (Vector2)player.position;
+    }
+
+    float size = 40f;
+    float parallax = 1f;
+
+    void Update () {
+        transform.position = new Vector3(0f, player.position.y, 0f);
+
+        meshRenderer.material.mainTextureOffset = 
+            new Vector2(0f, transform.position.y / size / parallax);
+
+    }
+}
